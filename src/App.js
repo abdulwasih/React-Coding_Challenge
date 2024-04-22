9fe0d4032344bd95a9c823fc751bd7818c80dd6b
Abdul Wasih <34627607+abdulwasih@users.noreply.github.com>
2024-04-22 22:34:07 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "./styles.css";
+import { useState } from "react";
+
+export default function App() {
+  const [topic, setTopic] = useState("");
+  const [topicList, setTopicList] = useState([]);
+
+  return (
+    <div className="App">
+      <h1>To do App</h1>
+      <form>
+        <input
+          type="text"
+          value={topic}
+          onChange={(event) => {
+            setTopic(event.target.value);
+          }}
+          placeholder="Enter the topic"
+        ></input>
+        <button>Add</button>
+      </form>
+      {topic}
+    </div>
+  );
+}
